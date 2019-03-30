ce6c15bc333a10abd811855cc4c8cb8c90f5e707
ice8scream <34999953+ice8scream@users.noreply.github.com>
2019-03-30 18:26:08 +0300

    task 10

---
+++ task-10/js/script.js
@@ -0,0 +1,96 @@
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+    let tabs = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+        
+    tabContent.forEach(function(item, index){
+        if(index != 0){
+            item.classList.add('hide');
+        }else {
+            item.classList.add('show');
+        }
+    });
+
+    function tabsFade(currenTab) {
+        tabContent.forEach(function(item, index){
+            if(index == currenTab){
+                if(item.classList.contains('hide')){
+                    item.classList.remove('hide');
+                    item.classList.add('show');
+                }
+            } else {
+                if(item.classList.contains('show')){
+                    item.classList.remove('show');
+                    item.classList.add('hide');
+                }
+
+            }
+        });
+    }
+
+    function getCurrentTab(element){
+        let currenTab;
+        for(currenTab = 0; currenTab < tabs.length; currenTab++){
+            if(tabs[currenTab] === element){
+                break;
+            }
+        }
+        tabsFade(currenTab);
+    }
+
+    info.addEventListener('click', function(event){
+        if(event.target.classList.contains('info-header-tab')){
+            getCurrentTab(event.target);
+        }  
+    });
+
+    //Timer
+
+    let deadline = '2019-03-31';
+
+    function getTimeRemaining(endtime) {
+        let dateDiffirent = Date.parse(endtime) - Date.parse(new Date());
+        if (dateDiffirent <= 0) {
+            return {
+                'total': 0,
+                'hours': 0,
+                'minutes': 0,
+                'seconds': 0
+            };
+        }
+        let seconds = Math.floor((dateDiffirent / 1000) % 60),
+            minutes = Math.floor((dateDiffirent / (1000 * 60)) % 60),
+            hours = Math.floor((dateDiffirent / (1000 * 60 * 60)));
+
+        return {
+            'total': dateDiffirent,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours < 10 ? '0' + t.hours : t.hours;
+            minutes.textContent = t.minutes < 10 ? '0' + t.minutes : t.minutes;
+            seconds.textContent = t.seconds < 10 ? '0' + t.seconds : t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+
+    }
+
+    setClock('timer', deadline);
+});
